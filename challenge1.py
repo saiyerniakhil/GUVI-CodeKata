58777f075cfbf700c1b14001902f8319daf3a8df
Sai Yerni Akhil <32711472+saiyerniakhil@users.noreply.github.com>
2019-03-20 16:06:16 +0530

    Update challenge1.py

--- challenge1.py
+++ challenge1.py
@@ -1,11 +1,11 @@
 try:
 	x=int(input())
 	if(x > 0):
-		print('Positive')
+		print('Positive',end="")
 	elif(x < 0):
-		print('Negative')
+		print('Negative',end="")
 	else:
-		print('Zero')
+		print('Zero',end="")
 except:
-	print("Error")
+	print("Error",end="")
 
