a87064c8b2188e3790498756af4898cb82187b88
saiyerniakhil <saiyerniakhil>
2019-03-23 12:04:27 +0530

    Add challenge8.py

--- challenge8.py
+++ challenge8.py
@@ -1,3 +1,6 @@
-x = int(input())
-sum = (x* (x + 1)) / 2
-print(int(sum),end="")
+try:
+    x = int(input())
+    sum = (x* (x + 1)) / 2
+    print(int(sum),end="")
+except:
+    print('invalid',end="")
