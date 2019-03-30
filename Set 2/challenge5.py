559c35d07e71644a8387bcf3d9ae56d18baf9c8d
saiyerniakhil <saiyerniakhil>
2019-03-31 00:07:54 +0530

    update Set 2/challenge5.py

--- Set 2/challenge5.py
+++ Set 2/challenge5.py
@@ -4,9 +4,9 @@ try:
     for i in range(n+1,q):
         if ( i % 2 == 0):
             if not(i == q-1):
-                print(i,end=" ")
-            else:
                 print(i,end="")
+            else:
+                print(i,end=" ")
         else:
             continue
 except:
