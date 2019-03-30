0766d01c3a82e933de9362c03498f47ff28f9790
saiyerniakhil <saiyerniakhil>
2019-03-31 00:01:06 +0530

    Add Set 2/challenge5.py

---
+++ Set 2/challenge5.py
@@ -0,0 +1,13 @@
+try:
+    x = input().split(" ")
+    n,q = int(x[0]), int(x[1])
+    for i in range(n+1,q):
+        if ( i % 2 == 0):
+            if not(i == q-1):
+                print(i,end=" ")
+            else:
+                print(i,end="")
+        else:
+            continue
+except:
+    print("invalid",end="")
