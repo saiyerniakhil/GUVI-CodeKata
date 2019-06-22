ab1a8e440d6bf9bd985d0a2110df3ba3c0535617
saiyerniakhil <saiyerniakhil>
2019-06-22 18:22:47 +0530

    Update Set2/Challenge8.py

--- Set 2/challenge8.py
+++ Set 2/challenge8.py
@@ -7,13 +7,19 @@ try:
         for j in str(i):
             sum += int(j) * int(j) * int(j)
 
-            if(sum == int(i)):
-                arms.append(i)
-            else:
-                pass
+        if(sum == int(i)):
+            arms.append(i)
+        else:
+            pass
 
-    for i in arms:
-        print(i,end="")
+
+    for i in range(len(arms)):
+        if(i == len(arms)-1):
+            print(arms[i],end="")
+        else:
+            print(arms[i],end=" ")
+
+        
 except:
     print("please enter integers")
 
