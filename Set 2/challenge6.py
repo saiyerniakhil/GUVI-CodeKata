c82c8d5c1d068fe8f129c47a34a02979b0996ced
saiyerniakhil <saiyerniakhil>
2019-06-22 17:34:51 +0530

    Update Set2/Challenge 6

--- Set 2/challenge6.py
+++ Set 2/challenge6.py
@@ -4,7 +4,7 @@ try:
 	x,y = int(x[0]), int(x[1])
 	primes= []
 
-	for i in range(x,y+1):
+	for i in range(x,y):
 			count = 0
 			for j in range(1,i+1):
 					if(i % j == 0):
@@ -14,8 +14,8 @@ try:
 
 	for i in range(0,len(primes)):
 			if(i == len(primes)-1):
-				print(i,end="")
+				print(primes[i],end="")
 			else:
-				print(i,end=" ")
+				print(primes[i],end=" ")
 except:
 	print("Please enter integers only")
