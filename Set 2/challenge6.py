8d02f321622951d21272abddda5b1260389cfdc2
saiyerniakhil <saiyerniakhil>
2019-06-22 17:27:02 +0530

    Add Set2/Challenge 6

---
+++ Set 2/challenge6.py
@@ -0,0 +1,18 @@
+x = input().split(" ")
+
+x,y = int(x[0]), int(x[1])
+primes= []
+
+for i in range(x,y+1):
+        count = 0
+        for j in range(1,i+1):
+                if(i % j == 0):
+                        count += 1
+        if(count == 2):
+                primes.append(i)
+
+for i in range(0,len(primes)):
+		if(i == len(primes)-1):
+			print(i,end="")
+		else:
+        	print(i,end=" ")
