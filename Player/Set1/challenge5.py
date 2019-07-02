7a16579c315c5d4e394c8c15ccd70322a9ba1b3c
saiyerniakhil <makhil123@yahoo.in>
2019-07-02 18:48:27 +0530

    Update changes

--- Player/Set1/challenge5.py
+++ Player/Set1/challenge5.py
@@ -1,48 +1,14 @@
-x = int(input())
-
-roman = ''
-four = 'IV'
-def less_than_4(x):
-    roman = ''
-    roman += 'I' * x
-    return roman
-
-def between_5_and_9(x):
-    roman = 'V'
-    roman = roman + (x-5) * 'I'
-    return roman
-
-def between_10_and_21(x):
-    roman = 'X'
-    if (x-10 in range(4)):
-        roman += less_than_4(x-10) 
-    elif(x-10 == 4):
-        roman += 'IV'
-    elif(x-10 == 9):
-        roman += 'IX'
-    elif(x-10 in range(5,10)):
-        roman += between_5_and_9(x-10)
-    elif(x-10 == 10):
-        roman += 'X'
-
-    return roman
-
-if(x in range(1,4)):
-    res = less_than_4(x)
-elif(x == 4):
-    res = 'IV'
-elif( x in range(5,8)):
-    res = between_5_and_9(x)
-elif( x == 9 ):
-    res = 'IX'
-elif(x in range(10,21)):
-    res = between_10_and_21(x)
-
-
-print(res,end="")
-
-
-    
-    
-
-print(roman)
+val = input()
+dic = {
+    1 : "I",
+    2 : 'II',
+    3 : 'III',
+    4 : 'IV',
+    5 : 'V',
+    6 : 'VI',
+    7 : 'VII',
+    8 : 'VIII',
+    9 : 'IX',
+    10 : 'X',
+    11 : 'XI'
+}
