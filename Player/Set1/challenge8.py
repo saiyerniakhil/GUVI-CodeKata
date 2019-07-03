142dcc399696add1614663a340e27784664672a7
saiyerniakhil <makhil123@yahoo.in>
2019-07-03 10:54:23 +0530

    update Player/Set1/challenge8.py

--- Player/Set1/challenge8.py
+++ Player/Set1/challenge8.py
@@ -1,18 +1,17 @@
 
-x = input().split()
-
 def camelCase(x):
     f = ''
     f += (x[0].upper())
     f += x[1:]
     return f
 
-if(x.isalpha()):
-    for i in range(len(x)):
-        if ( i == len(x) - 1):
-            print(camelCase(x[i]),end="")
-        else:
-            print(camelCase(x[i]),end=" ")
-else:
-    print("please enter strings only!")
+
+x = input().split()
+
+for i in range(len(x)):
+    if ( i == len(x) - 1):
+        print(camelCase(x[i]),end="")
+    else:
+        print(camelCase(x[i]),end=" ")
+
 
