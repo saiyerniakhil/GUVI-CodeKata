3ad4fae6617e29e02f0dd89b9ed2e04d87103c49
saiyerniakhil <makhil123@yahoo.in>
2019-07-07 22:53:45 +0530

    Add challenges in set3

--- player_set3_1.py
+++ player_set3_1.py
@@ -4,7 +4,7 @@ x = sys.stdin.readlines()
 
 x = [i.strip("\n").split(" ") for i in x]
 
-print(x)
+#print(x)
 if (x[0][0] == x[1][0] == x[2][0]):
     print("yes")
 elif (x[0][1] == x[1][1] == x[2][1]):
